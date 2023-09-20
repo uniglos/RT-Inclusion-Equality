487ab02b8284633a53ab8041b6f5d50733739e16
Sam Puerta Terron <spuertaterron@gmail.com>
2023-09-20 15:31:19 +0100

    Added Node UI

--- Equality Project/Assets/Scripts/Editor/DialogueNodeEditor.cs
+++ Equality Project/Assets/Scripts/Editor/DialogueNodeEditor.cs
@@ -1,17 +1,59 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEditor;
 using UnityEngine;
 using XNodeEditor;
+using UnityEngine.UIElements;
+using Unity.VisualScripting;
 
 [CustomNodeEditor(typeof(DialogueNode))]
 public class DialogueNodeEditor : BaseNodeEditor {
+
+    private int _speechFieldHeight = 50;
+    private bool _isExpanded = false;
 
     public override void OnBodyGUI() {
         base.OnBodyGUI();
 
         DialogueNode node = (DialogueNode)target;
 
-        //EditorGUILayout.Popup(0, DialogueNode.characters);
+        serializedObject.Update();
+
+        SerializedProperty characterName = serializedObject.FindProperty("characterName");
+        SerializedProperty speech = serializedObject.FindProperty("speech");
+
+        EditorGUILayout.PropertyField(characterName, new GUIContent("Character Name"));
+
+        EditorGUI.BeginChangeCheck();
+        EditorGUILayout.PropertyField(speech, new GUIContent("Speech"), GUILayout.Height(_speechFieldHeight));
+
+        //If the user has clicked on the property field
+        if(GUIUtility.hotControl != 0 && !_isExpanded) {
+            _speechFieldHeight = 200;
+            _isExpanded = true;  
+        }
+
+        //Get the key event and check if the user has pressed enter
+        Event e = Event.current;
+
+        if (e.keyCode == KeyCode.Return) {
+            //Check if the speech box is expanded if true reset vars
+            if (GUIUtility.hotControl == 0 && _isExpanded) {
+                _speechFieldHeight = 50;
+                _isExpanded = false;
+            }
+        }
+
+        serializedObject.ApplyModifiedProperties();
     }
+}
+
+
+public class NodeDialogueWindow : EditorWindow {
+
+    public static void Open(SerializedProperty property) {
+
+    }
+
 }
