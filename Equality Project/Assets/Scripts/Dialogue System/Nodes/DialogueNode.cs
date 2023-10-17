93f773570f7e34e67743f5ce88684ea913e86e60
JakeHood <Jakehood03@hotmail.com>
2023-10-17 15:59:03 +0100

    -xnodeeditor

--- Equality Project/Assets/Scripts/Dialogue System/Nodes/DialogueNode.cs
+++ Equality Project/Assets/Scripts/Dialogue System/Nodes/DialogueNode.cs
@@ -3,7 +3,6 @@ using System.Collections;
 using System.Collections.Generic;
 using Unity.VisualScripting;
 using UnityEngine;
-using XNodeEditor;
 
 namespace Dialogue {
     public class DialogueNode : BaseNode {
